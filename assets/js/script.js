6d183c72994331d995eae893d73dbd41d0196ee7
ellisa <ellisaissa@gmail.com>
2021-03-04 21:19:18 -0500

    in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,18 @@ $(document).ready(function () {
       });
     });
   });
+
+  var modal = document.getElementById("results-modal");
+  var btn = document.getElementById("modal btn");
+  var span = document.getElementsByClassName("close") [0];
+  btn.click = function () {
+    modal.style.display = "block";
+  }
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display ="none";
+    }
+  }
