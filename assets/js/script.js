ea5bbcf1993d50a4fe6a5ae4f71b3d28d5d69f81
Jon Yeatman <jonlyeatman@gmail.com>
2021-03-04 20:56:15 -0500

    HTML notes button IDs JS random selection meal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// navbar
 $(document).ready(function () {
     $(".nav-toggler").each(function (_, navToggler) {
       var target = $(navToggler).data("target");
@@ -8,3 +9,54 @@ $(document).ready(function () {
       });
     });
   });
+
+//   API meal db
+  function searchApi(query, format) {
+    let locQueryUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
+  
+    if (format) {
+      locQueryUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
+    }
+  
+    fetch(locQueryUrl)
+      .then(function (response) {
+        if (!response.ok) {
+          throw response.json();
+        }
+  
+        return response.json();
+      })
+      .then(function (locRes) {
+        // write query to page so user knows what they are viewing
+        resultTextEl.textContent = locRes.search.query;
+  
+        console.log(locRes);
+  
+        if (!locRes.results.length) {
+          console.log('No results found!');
+          resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
+        } else {
+          resultContentEl.textContent = '';
+          for (var i = 0; i < locRes.results.length; i++) {
+            printResults(locRes.results[i]);
+          }
+        }
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
+
+  function handleSearchFormSubmit(event) {
+    event.preventDefault();
+  
+    var searchInputVal = document.querySelector('#search-input').value;
+    var formatInputVal = document.querySelector('#format-input').value;
+  
+    if (!searchInputVal) {
+      console.error('You need a search input value!');
+      return;
+    }
+  
+    searchApi(searchInputVal, formatInputVal);
+  }
