5b6ee368831f85e423f9fe5c421c57a445a0e6d1
Valerie Russell <vruss14@gmail.com>
2021-03-06 10:56:53 -0500

    created random taco btn that fetches API data

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,66 @@ $(document).ready(function () {
       });
     });
   });
+
+  // Script for the button that generates a random restaurant response
+
+  let btnDiv = $("#btn-test");
+
+  let tacoBaseLayer = $("#base-layer");
+  let tacoBaseLayerLink = $("#base-layer-link");
+
+  let tacoCondiment = $("#condiment");
+  let tacoCondimentLink = $("#condiment-link");
+
+  let tacoMixin = $("#mixin");
+  let tacoMixinLink = $("#mixin-link");
+
+  let tacoSeasoning = $("#seasoning");
+  let tacoSeasoningLink = $("#seasoning-link");
+
+  let tacoShell = $("#shell");
+  let tacoShellLink = $("#shell-link");
+
+  let randomBtnTaco = $("<button>");
+  randomBtnTaco.text("Forget it, I just want tacos");
+  randomBtnTaco.attr('class', "random-taco");
+  btnDiv.append(randomBtnTaco);
+
+  randomBtnTaco.on("click", function () {
+    console.log("Click event works");
+    let requestRandomTaco = "http://taco-randomizer.herokuapp.com/random/";
+
+    fetch(requestRandomTaco)
+      .then(function(response) {
+        return response.json();
+      })
+
+      .then(function(response) {
+        console.log(response);
+
+        tacoBaseLayer.text(response.base_layer.name);
+        tacoBaseLayer.attr("style", "color: blue; font-weight: bold; text-decoration: underline;");
+        tacoBaseLayerLink.attr("href", response.base_layer.url);
+        tacoBaseLayerLink.attr("target", "_blank");
+
+        tacoCondiment.text(response.condiment.name);
+        tacoCondiment.attr("style", "color: blue; font-weight: bold; text-decoration: underline;");
+        tacoCondimentLink.attr("href", response.condiment.url);
+        tacoCondimentLink.attr("target", "_blank");
+     
+        tacoMixin.text(response.mixin.name);
+        tacoMixin.attr("style", "color: blue; font-weight: bold; text-decoration: underline;");
+        tacoMixinLink.attr("href", response.mixin.url);
+        tacoMixinLink.attr("target", "_blank");
+
+        tacoSeasoning.text(response.seasoning.name);
+        tacoSeasoning.attr("style", "color: blue; font-weight: bold; text-decoration: underline;");
+        tacoSeasoningLink.attr("href", response.seasoning.url);
+        tacoSeasoningLink.attr("target", "_blank");
+
+        tacoShell.text(response.shell.name);
+        tacoShell.attr("style", "color: blue; font-weight: bold; text-decoration: underline;");
+        tacoShellLink.attr("href", response.shell.url);
+        tacoShellLink.attr("target", "_blank");
+      })
+  })
